7048d619b5ff70f51aae07dcb2abc89bb703df28
Jani Repo <jani.repo@student.lut.fi>
2021-10-06 12:11:03 +0300

    First

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const os = require("os");
+const path = require("path");
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+//app.use(express.urlencoded({extended: false}));
+
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World!</h1> Hello and world world");
+    let testUrl = req.url;
+
+});
+
+app.get("/recipe/:food", (req, res) => {
+    let testUrl = req.url;
+    console.log(testUrl);
+});
+
+//app.use("/api/poems", require("./api/poems.js"));
+
+
+app.listen(port, () => console.log(`Server listening a port ${port}!`));
