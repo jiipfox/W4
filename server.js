aaf668c6f7dc68ab41c98c9fafe1f42c5c34b2bb
Jani Repo <jani.repo@student.lut.fi>
2021-10-06 15:47:06 +0300

    HGmm

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+//const { response } = require("express");
 const express = require("express");
 const os = require("os");
 const path = require("path");
@@ -6,14 +6,15 @@ const app = express();
 const port = 1234;
 
 app.use(express.json());
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 //app.use(express.urlencoded({extended: false}));
-
-
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1> Hello and world world");
+    res.send("<h1>Hello World!</h1> Hello and world world3");
     let testUrl = req.url;
 
 });
@@ -27,7 +28,13 @@ app.get("/recipe/:food", (req, res) => {
     console.log(dummyJson);
 
     const recipeJson = JSON.parse(dummyJson);
-    res.send(recipeJson);
+
+    res.render('index', { title: 'Recipe gt', 
+                            name: recipeJson.name, 
+                            incredients: recipeJson.incredients, 
+                            instructions: recipeJson.instructions});
+
+    //res.send(recipeJson);
 });
 
 //app.use("/api/poems", require("./api/poems.js"));
